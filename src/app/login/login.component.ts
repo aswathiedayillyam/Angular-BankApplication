e35e1ada2cc66b88739eb1c5fa0528374ae98e9c
aswathi <aswathinaire2gmail.com>
2021-05-27 11:14:24 -0700

    dashboard

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,17 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
   aim="Your Perfect Banking Partner"
-  account="Account Number Please"
-  accno="";
+  accno="Account Number Please";
+  pswd="";
   AccountDetails:any= {
     1000: { accno: 1000, pswd: "user1", balance: 5000, username:"user1"},
     1001: { accno: 1001, pswd: "user2", balance: 3000, username:"user2"},
     1002: { accno: 1002, pswd: "user3", balance: 10000, username:"user3"},
     1003: { accno: 1003, pswd: "user4", balance: 15000, username:"user4"},
 }
-  pswd: any;
 
-  constructor() { }
+
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
   }
@@ -33,16 +34,17 @@ export class LoginComponent implements OnInit {
 
   }
 
-  login(a:any,p:any){
+  login(){
     
-    var accno=a.value;
-    var pswd=p.value;
+    var accno=this.accno;
+    var pswd=this.pswd;
 
     let dataset = this.AccountDetails;
 
     if (accno in dataset) {
         if (pswd == dataset[accno]["pswd"]) {
             alert("Login Successful");
+            this.router.navigateByUrl('dashboard');
         }
         else {
           alert("Invalid Password");
